ba922069a37101d9ee7e265dedfb6bafb308675c
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2021-10-23 20:24:07 +0530

    Navbar component completed!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
+
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Routes } from './components/Routes';
 
 const App = () => {
-  const [darkTheme,isDarkTheme] = useState(false);
+  const [darkTheme,setdarkTheme] = useState(false);
   return (
     <div className={darkTheme?'dark':''}>
       <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
-        <Navbar />
+        <Navbar darkTheme={darkTheme} setdarkTheme={setdarkTheme} />
         <Routes />
         <Footer />
       </div>
